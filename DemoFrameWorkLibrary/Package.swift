4e7a10b7466743bb49b8786ff2f8d3f632b1525c
Ranjit Pal <ranjit.pal@codiot.com>
2025-07-30 17:43:36 +0530

    - initial commite

---
+++ DemoFrameWorkLibrary/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DemoFrameWorkLibrary",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "DemoFrameWorkLibrary",
+            targets: ["DemoFrameWork"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+//        .target(
+//            name: "DemoFrameWorkLibrary"),
+        .binaryTarget(name: "DemoFrameWork", path: "./Users/apple/Projects/SDK/DemoFrameWork/DemoFrameWork.xcframework")
+
+    ]
+)
